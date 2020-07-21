811b724bacd44386bf3d3b640983980df4bbb4da
Kyle Coop <kylecoop152@gmail.com>
2020-07-22 00:51:57 +0100

    Update style, add current date using moment.js

--- thingie.js
+++ thingie.js
@@ -2,6 +2,7 @@ function updTime()
 {
 	const days = 1000 * 60 * 60 * 24
 	let infoElem = document.getElementById("info")
+	let dateElem = document.getElementById("dateDisplay")
 	let now = new Date();
 	let then = new Date(2020, 6, 20);
 	let diff = now - then;
@@ -16,5 +17,6 @@ function updTime()
 		infoElem.innerHTML = "NO";
 		infoElem.style.color = "green";
 	}
+	dateElem.innerHTML = "(on " + moment().format("Do [of] MMMM, YYYY") + ")";
 }
 updTime();
