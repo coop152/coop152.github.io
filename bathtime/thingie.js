a5a643949a4b1733a0759c337048a4fd30ab012e
coop152 <40497580+coop152@users.noreply.github.com>
2021-09-20 22:24:23 +0100

    update bafftime script
    minor fixes (add semicolons for consistency, rename constant and add comment)
    change day (changed the day i shower on xd)

--- bathtime/thingie.js
+++ bathtime/thingie.js
@@ -1,12 +1,12 @@
 function updTime() 
 {
-	const days = 1000 * 60 * 60 * 24
-	let infoElem = document.getElementById("info")
-	let dateElem = document.getElementById("dateDisplay")
+	const day = 1000 * 60 * 60 * 24 // number of milliseconds in a day
+	let infoElem = document.getElementById("info");
+	let dateElem = document.getElementById("dateDisplay");
 	let now = new Date();
-	let then = new Date(2020, 6, 20);
+	let then = new Date(2020, 6, 21);
 	let diff = now - then;
-	diff = diff / days
+	diff = diff / day;
 	if (diff % 2 < 1) 
 	{
 		infoElem.innerHTML = "YES";
